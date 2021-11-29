df737ba4b1931ae93bce5eb623cf1b76acee39d1
Farruhbey <farruhbeyrixsiboyev05@gmail.com>
2021-11-29 17:48:13 +0500

    s

--- academiya/settings.py
+++ academiya/settings.py
@@ -48,10 +48,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'loyiha'
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
