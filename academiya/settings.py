dc1ba116afdf777bd3cd2fdf2270cb396b97c03c
Farruhbey <farruhbeyrixsiboyev05@gmail.com>
2021-12-01 19:04:44 +0500

    fix: cors errors

--- academiya/settings.py
+++ academiya/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ["127.0.0.1","academy.uz","81.95.232.205"]
 ALLOWED_HOSTS=['*']
 CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -42,15 +43,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'drf_yasg',
-    'corsheaders',
+    
     'loyiha'
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'academiya.urls'
